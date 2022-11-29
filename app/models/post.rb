aa37cc1e44e1780a010b276d40b006e60157f6e4
Mariam Ramirez <zarifers@gmail.com>
2022-11-29 14:49:04 +0100

    installed gem geocode, post model updated, units chenged to km, added columns to schema for latitude and longitude

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_many :bookmarks
   has_many :votes
   has_many :comments
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
