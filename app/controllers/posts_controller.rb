c2c244e10f0e6347ff356d65ce1d338b6ce4e2ad
MaxSeck <maxseck@hotmail.de>
2022-11-29 17:49:09 +0100

    Posts Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+  def index
+    if params[:query].present?
+      sql_query = "description ILIKE :query OR content ILIKE :query OR address ILIKE :query"
+      @posts = Post.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @posts = Post.all
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.save!
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :address, :title)
+  end
 end
