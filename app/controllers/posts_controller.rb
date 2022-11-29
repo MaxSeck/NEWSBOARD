0e6d4170f9dce05e524d7ebc1f19eac498588812
Mariam Ramirez <zarifers@gmail.com>
2022-11-29 16:58:01 +0100

    add map css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+    def index
+      @posts = Post.all
+      @markers = @posts.geocoded.map do |post|
+        {
+          lat: post.latitude,
+          lng: post.longitude,
+          info_window: render_to_string(partial: "info_window", locals: {post: post})
+        }
+      end
 end
