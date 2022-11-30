c0fd70acca7dcb4b08456fd3ad9ff956f3ac3af2
John Sagittary <johnsagittary@gmail.com>
2022-11-30 15:30:04 +0100

    It Fng Works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content, :address, :title)
+    params.require(:post).permit(:content, :address, :title, :photo)
   end
 end
