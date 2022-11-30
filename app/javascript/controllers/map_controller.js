59177876601a83c4b0e4f6fa9d61c0d1d0ea9472
Mariam Ramirez <zarifers@gmail.com>
2022-11-30 15:06:12 +0100

    added map in home page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -9,26 +9,37 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
+
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-  }
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
 
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
+  }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+      // const customMarker = document.createElement("div")
+      //   customMarker.className = "marker"
+      //   customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      //   customMarker.style.backgroundSize = "contain"
+      //   customMarker.style.width = "25px"
+      //   customMarker.style.height = "25px"
+
+      // new mapboxgl.Marker(customMarker)
+      // .setLngLat([marker.lng, marker.lat])
+      // .setPopup(popup)
+      // .addTo(this.map)
       new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
-        .addTo(this.map)
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // Add this
+      .addTo(this.map)
     });
   }
-
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
