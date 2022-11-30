6f0818fe932039f762a49074248b4a4db94446af
MaxSeck <maxseck@hotmail.de>
2022-11-30 15:57:19 +0100

    Post destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get "votes/new"
+  get "votes/create"
   devise_for :users
   root to: "pages#home"
- 
+
   get "posts/new", to: "posts#new"
   post "posts", to: "posts#create"
 
-  resources :posts, only: [:index, :show, :new, :create]
+
+
+  resources :posts, only: [:index, :show, :new, :create, :destroy]
 
 end
 
