56a21d31520a6c16e6e48370ec463c677172c218
MaxSeck <maxseck@hotmail.de>
2022-12-02 10:15:35 +0100

    SeedDebug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+Comment.destroy_all
 Post.destroy_all
 User.destroy_all
-Comment.destroy_all
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
@@ -11,9 +11,6 @@ require "open-uri"
 require 'faker'
 
 puts "Cleaning database..."
-
-User.destroy_all
-Post.destroy_all
 
 puts "Creating Users"
 
