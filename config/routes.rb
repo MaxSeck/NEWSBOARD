6e3cbd25113a02f8ce7c7b3b467895b1387bee59
John Sagittary <johnsagittary@gmail.com>
2022-12-05 14:21:58 +0100

    basic comment functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :posts, only: [:index, :show, :new, :create, :destroy] do
-    resources :comments, only: [:index, :show, :new, :create, :destroy]
+    resources :comments, only: [:create]
   end
 end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
