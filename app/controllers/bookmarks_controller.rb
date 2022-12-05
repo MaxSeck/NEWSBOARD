fac9a992dd4ba4cb3c0f51b35d3ed0944026644f
Mariam Ramirez <zarifers@gmail.com>
2022-12-05 21:15:33 +0100

    added delete link

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,12 @@
 class BookmarksController < ApplicationController
-
   def index
     @bookmarks = current_user.bookmarks # array of bookmark objects
     @posts = @bookmarks.map { |bookmark| bookmark.post }
   end
 
-  def bookmarked_by?(user)
-    return true if bookmarks.any? {|b| b.user == user }
-  end
-
   def destroy
-    post = Post.find(params[:post_id])
-    bookmark = current_user.bookmarks.find(params[:id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_path, notice: "Deleted succefully", status: :see_other
   end
 end
